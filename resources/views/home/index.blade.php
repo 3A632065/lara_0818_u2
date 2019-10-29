185a5b9006e3af6ca38827cf4426c56fb4347cc6
xinru <s3a632065@student.ncut.edu.tw>
2019-10-29 22:17:42 +0800

    練習2-2 建立子樣板

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,18 @@
+<!--
+<!doctype	html>
+<html>
+    <head></head>
+    <body>
+        <h1>Home</h1>
+    </body>
+</html>
+-->
+
+@extends('layouts.master')
+
+@section('title','Home')
+
+@section('content')
+    <h1>Home</h1>
+    <p>This is my body content.</p>
+@endsection
